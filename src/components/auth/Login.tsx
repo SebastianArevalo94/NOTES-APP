1f0aab709692394c51a00dc74e65d6d4f946f0af
SEBASTIAN AREVALO SALINAS <sebastian.arevalo@aviatur.com.co>
2022-08-27 12:12:25 -0500

    login-1.0

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,26 @@
+import { LoginSVG } from "../assets/images";
+import "../styles/components/Login/login.css";
+
+export const Login = () => {
+  return (
+    <div className="global">
+      <div className="login-logo">
+        <LoginSVG />
+      </div>
+      <div className="login-container">
+        <div>
+          <img src="https://iili.io/4TAgRt.png" alt="" width="270px" />
+        </div>
+        <div className="login">
+          <h2>Notes App</h2>
+          <form className="login-form">
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Email" />
+            <button type="submit">Go</button>
+          </form>
+        </div>
+        <p className="login-text">Don’t have an account? <br/> Create One here</p>
+      </div>
+    </div>
+  );
+};
